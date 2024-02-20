444622d7253ad8dd968d7319596aea4963d28bb9
gukenji <gustavokenjitakahashi@gmail.com>
2024-02-20 17:28:35 -0300

    task 21 complete

--- contracts/20. Structs.sol
+++ contracts/20. Structs.sol
@@ -1,3 +1,38 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
+contract Structs {
+    struct Car {
+        string model;
+        uint year;
+        address owner;
+    }
+
+    Car public car;
+    Car[] public cars;
+    mapping(address => Car[]) public carsByOwner;
+
+    function examples() external {
+        Car memory toyota = Car("toyota", 1990, msg.sender);
+
+        Car memory lambo = Car({model:"lamborghini", year: 1980, owner: msg.sender});
+
+        Car memory tesla;
+        tesla.model = "tesla";
+        tesla.year = 1910;
+        tesla.owner = msg.sender;
+
+
+        cars.push(toyota);
+        cars.push(lambo);
+        cars.push(tesla);
+
+        cars.push(Car("Ferrari", 1920, msg.sender));
+
+        Car storage _car = cars[0];
+        _car.year = 1930;
+
+        delete _car.owner;
+        delete cars[1];
+    }
+}
---
+++ contracts/21. Enum.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract Enum {
+    enum Status {
+        None,
+        Pending,
+        Shipped,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
+
+    struct Order {
+        address buyer;
+        Status status;
+    }
+
+    Order[] public orders;
+
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    function set(Status _status) external {
+        status = _status;
+    }
+
+    function ship() external {
+        status = Status.Shipped;
+
+    }
+
+    function reset() external {
+        delete status; // default value = first item of status == None;
+
+    }
+}
